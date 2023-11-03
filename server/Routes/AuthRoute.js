39a3b44db067430056890d6b2a8c66f60b6e3cb3
bhavani <bhavanikodur14@gmail.com>
2023-11-03 18:28:19 +0400

    added code

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -3,16 +3,11 @@ const {userVerification} = require('../Middlewares/AuthMiddleware');
 
 const router = require("express").Router();
 
-
-
-
  router.post('/', userVerification);
-// router.post('/', function(req, res){
-//     userVerification
-//   });
+
 router.post("/signup", Signup);
 router.post('/login', Login);
-// router.post('/',userVerification);
+
 console.log(typeof userVerification);
 
 module.exports = router;
